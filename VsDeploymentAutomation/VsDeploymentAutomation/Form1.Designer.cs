6d600ff2d825c12ceb4ef9c5bc3f44b55f8913ac
Tanjeer <tbh.tilok@live.com>
2021-03-13 21:40:02 +0600

    committed with first project setup

---
+++ VsDeploymentAutomation/VsDeploymentAutomation/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace VsDeploymentAutomation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(440, 194);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 275);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.runButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ VsDeploymentAutomation/VsDeploymentAutomation/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VsDeploymentAutomation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            Publish();
+        }
+
+        #region [Custom Private Functions]
+
+        private bool Pull()
+        {
+            ProcessStartInfo gitInfo = new ProcessStartInfo();
+            gitInfo.CreateNoWindow = true;
+            gitInfo.RedirectStandardError = true;
+            gitInfo.RedirectStandardOutput = true;
+            gitInfo.UseShellExecute = false;
+            gitInfo.FileName = @"C:\Program Files\Git\bin\git.exe";
+
+            Process gitProcess = new Process();
+            gitInfo.Arguments = "pull origin master"; // such as "fetch orign"
+            gitInfo.WorkingDirectory = @"C:\git\public\ndate";
+
+            gitProcess.StartInfo = gitInfo;
+            gitProcess.Start();
+
+            string stderr_str = gitProcess.StandardError.ReadToEnd();  // pick up STDERR
+            string stdout_str = gitProcess.StandardOutput.ReadToEnd(); // pick up STDOUT
+
+            textBox1.Text = stdout_str + stderr_str;
+
+            gitProcess.WaitForExit();
+            gitProcess.Close();
+
+            return true;
+        }
+
+        private bool Build()
+        {
+            ProcessStartInfo gitInfo = new ProcessStartInfo();
+            gitInfo.CreateNoWindow = true;
+            gitInfo.RedirectStandardError = true;
+            gitInfo.RedirectStandardOutput = true;
+            gitInfo.UseShellExecute = false;
+            gitInfo.FileName = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe";
+
+            Process gitProcess = new Process();
+            gitInfo.Arguments = @"C:\git\public\Fractal\Fractal\Fractal.sln"; // such as "fetch orign"
+            //gitInfo.WorkingDirectory = @"C:\git\public\ndate";
+
+            gitProcess.StartInfo = gitInfo;
+            gitProcess.Start();
+
+            string stderr_str = gitProcess.StandardError.ReadToEnd();  // pick up STDERR
+            string stdout_str = gitProcess.StandardOutput.ReadToEnd(); // pick up STDOUT
+
+            textBox1.Text = stdout_str + stderr_str;
+
+            gitProcess.WaitForExit();
+            gitProcess.Close();
+
+            return true;
+        }
+
+        private bool Publish()
+        {
+            ProcessStartInfo gitInfo = new ProcessStartInfo();
+            gitInfo.CreateNoWindow = false;
+            gitInfo.RedirectStandardError = true;
+            gitInfo.RedirectStandardOutput = true;
+            gitInfo.UseShellExecute = false;
+            gitInfo.FileName = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe";
+
+            Process gitProcess = new Process();
+            gitInfo.Arguments = @"C:\git\private\armadareports\ArmadaReports\ArmadaReports.Web\ArmadaReports.Web.csproj /p:DeployOnBuild=true /p:PublishProfile=armadareports /p:VisualStudioVersion=10.0 /P:PublishProfileRootFolder=C:\git\private\armadareports\ArmadaReports\ArmadaReports.Web\Properties\PublishProfiles\armadareports.pubxml; // such as "fetch orign"
+            //gitInfo.WorkingDirectory = @"C:\git\public\ndate";
+
+            gitProcess.StartInfo = gitInfo;
+            gitProcess.Start();
+
+            string stderr_str = gitProcess.StandardError.ReadToEnd();  // pick up STDERR
+            string stdout_str = gitProcess.StandardOutput.ReadToEnd(); // pick up STDOUT
+
+            textBox1.Text = stdout_str + stderr_str;
+
+            gitProcess.WaitForExit();
+            gitProcess.Close();
+
+            return true;
+        }
+
+        #endregion
+    }
+}
